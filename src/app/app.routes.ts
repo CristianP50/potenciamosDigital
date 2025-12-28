b8bd9e5181956b545d9f41c4037ffd8f548c97f0
CristianP50 <cristianjp.padierna2@gmail.com>
2025-12-28 17:52:32 -0500

    Se agregan páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./pages/home/home').then(m => m.Home)
+    },
+    {
+        path: 'about-us',
+        loadComponent: () =>
+            import('./pages/about-us/about-us').then(m => m.AboutUs)
+    },
+    {
+        path: 'services',
+        loadComponent: () =>
+            import('./pages/services/services').then(m => m.Services)
+    },
+];
