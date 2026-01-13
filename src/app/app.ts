afb37bc7c0970ec888df575d0b7fb9f646b59826
CristianP50 <cristianjp.padierna2@gmail.com>
2026-01-12 19:09:19 -0500

    trabajando en la landing de hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './pages/home/home';
 import { Header } from './components/header/header';
 import { ContactButton } from './components/contact-button/contact-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header, ContactButton],
+  imports: [RouterOutlet, Header, ContactButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
